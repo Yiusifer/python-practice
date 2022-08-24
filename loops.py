622a97ebcc5c0125032ef701011a329bbd428516
Adam Yiu <adam_yiu@outlook.com>
2022-08-24 17:33:06 -0400

    while loops & string multiplication

---
+++ loops.py
@@ -0,0 +1,11 @@
+i = 1
+
+while i <= 5:
+  print(i)
+  i += 1
+
+# Recall that strings cannot be concatenated with numbers, but can be multiplied by numbers
+y = 2
+while y<= 10:
+  print(y * '*')
+  y += 2
