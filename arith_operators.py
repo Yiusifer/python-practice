d55410d758f8a8b39e1b03a6a300dc7bc4bd1c73
Adam Yiu <adam_yiu@outlook.com>
2022-08-24 16:58:28 -0400

    arithmetic & logical operators

---
+++ arith_operators.py
@@ -0,0 +1,7 @@
+print(10 + 3)
+
+# How many times can 3 go into 10
+print(10 // 3)
+
+# 10 ^ 3
+print(10 ** 3)
---
+++ logical_operators.py
@@ -0,0 +1,12 @@
+price = 25
+# Will return true
+print(price > 10 and price < 30)
+
+secondPrice = 5
+
+# Will return true
+print(secondPrice > 10 or secondPrice < 30)
+
+# will return true
+# 'not' is same as '!' in JS
+print(not secondPrice == 10)
